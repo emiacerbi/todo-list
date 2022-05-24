8289548583ebc835454705acb23d6f9e799cbeb0
Emiliano Acerbi <eacerbip@gmail.com>
2022-05-24 09:48:15 -0300

    finished mobile

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,25 +7,29 @@ interface Props {
 }
 
 export const TodoList = ({ todos, setTodos }: Props) => {
+  const [filter, setFilter] = useState('all')
+
   const filters = [
     { type: 'all', content: 'All' },
     { type: 'active', content: 'Active' },
     { type: 'completed', content: 'Completed' }
   ]
 
-  const [filter, setFilter] = useState('all')
-
-  const handleDelete = (id: string) => {
-    setTodos(todos.filter(todo => todo.id !== id))
+  const handleComplete = (id: string) => {
+    setTodos(todos.map(prevTodos => prevTodos.id === id ? { ...prevTodos, complete: !prevTodos.complete } : prevTodos))
   }
 
-  const handleComplete = (id: string) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, complete: !todo.complete } : todo))
+  const handleDelete = (id: string) => {
+    setTodos(todos.filter(prevTodos => prevTodos.id !== id))
+  }
+
+  const deleteAllComplete = () => {
+    setTodos(todos.filter(prevTodos => prevTodos.complete === false))
   }
 
   return (
     <>
-      <ul>
+      <ul className='todo-list' >
         {
           todos
             .filter(todo => filter === 'active'
@@ -36,26 +40,43 @@ export const TodoList = ({ todos, setTodos }: Props) => {
             .map(todo => {
               const { id, text } = todo
               return (
-                <li className='flex ai-center' key={id} >
+                <li className='flex ai-center todo-list__list-item ' key={id} >
                   <img src="../../src/assets/icon-check.svg" alt="" />
-                  <p onClick={() => handleComplete(id)}>
+                  <p className={`todo-list__list-item__text ${todo.complete && 'line-through'}`} onClick={() => handleComplete(id)}>
                     {text}
                   </p>
                   <span onClick={() => handleDelete(id)}>
-                    <img src="../../src/assets/icon-cross.svg" alt="" />
+                    <img className='cross' src="../../src/assets/icon-cross.svg" alt="cross" width={13} />
                   </span>
                 </li>
               )
             })
         }
+
+        <div className='todo-list__footer'>
+          {
+            todos.filter(todo => todo.complete === false).length
+          }
+          <span>
+            items left
+          </span>
+
+          <button onClick={deleteAllComplete}>
+            Clear completed
+          </button>
+        </div>
       </ul>
 
-      <footer>
+      <footer className='footer'>
         {
-          filters.map(filter => {
-            const { content, type } = filter
+          filters.map(fil => {
+            const { content, type } = fil
             return (
-              <button key={type} onClick={() => setFilter(type)}>
+              <button
+                key={type}
+                onClick={() => setFilter(type)}
+                className={`footer__btn ${type === filter && 'selected'}`}
+              >
                 {content}
               </button>
             )
